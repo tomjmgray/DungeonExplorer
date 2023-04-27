1ffe82c8814334a5e7dfb203f60d0d074f0f01c3
Tom Gray <tgray@placergroup.com>
2023-04-26 21:30:57 -0800

    adds minor changes to vite config for deployment testing

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+const ASSET_URL = process.env.ASSET_URL || '';
+
+
 export default defineConfig({
+  // base: `${ASSET_URL}/index.html`,
   plugins: [vue()],
+  server: {
+    '/': {
+      target: 'http://localhost:80'
+    }
+  }
 })
